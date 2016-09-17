3ff81e3b99244be42620e3990d4eb0f097bea3d6
Igor Hipólito Vieira <zignd.igor@gmail.com>
2016-09-17 20:14:34 -0300

    Refactored to add proper asynchronous parallel parsing of the documents

--- src/extension.ts
+++ src/extension.ts
@@ -12,39 +12,62 @@ import ParseEngineGateway from './parse-engine-gateway';
 let notifier: Notifier = new Notifier('html-css-class-completion.cache');
 let uniqueDefinitions: CssClassDefinition[];
 
-async function cache(): Promise<void> {
-    console.log('HTML CSS Class Completion: Looking for CSS classes on the workspace...');
-    notifier.notify('eye', 'Looking for CSS classes on the workspace...');
+function cache(): Promise<void> {
+    return new Promise<void>(async (resolve, reject): Promise<void> => {
+        try {
+            notifier.notify('eye', 'Looking for CSS classes on the workspace...');
 
-    let uris: vscode.Uri[] = await Fetcher.findAllParseableDocuments();
-    let definitions: CssClassDefinition[] = [];
+            console.log('Looking for parseable documents...');
+            let uris: vscode.Uri[] = await Fetcher.findAllParseableDocuments();
 
-    try {
-        for (let uri of uris) {
-            try {
-                Array.prototype.push.apply(definitions, await ParseEngineGateway.callParser(uri));
-            } catch (error) {
-                console.error(`HTML CSS Class Completion: Failed to cache css classes from "${uri}"`);
-            }
+            console.log('Found all parseable documents.');
+            let definitions: CssClassDefinition[] = [];
+
+            let failedLogs: string = '';
+            let failedLogsCount: number = 0;
+
+            console.log('Parsing documents and looking for CSS class definitions...');
+            return async.each(uris, async (uri, callback) => {
+                try {
+                    Array.prototype.push.apply(definitions, await ParseEngineGateway.callParser(uri));
+                    callback();
+                } catch (error) {
+                    failedLogs += `${uri.path}\n`;
+                    failedLogsCount++;
+                    callback();
+                }
+            }, (error) => {
+                if (error) {
+                    console.error('Failed to parse the documents: ', error);
+                    return reject(error);
+                }
+
+                uniqueDefinitions = _.uniqBy(definitions, (x) => x.className);
+
+                console.log('Sumary:');
+                console.log(uris.length, 'parseable documents found');
+                console.log(definitions.length, 'CSS class definitions found');
+                console.log(uniqueDefinitions.length, 'unique CSS class definitions found');
+                console.log(failedLogsCount, 'failed attempts to parse. List of the documents:');
+                console.log(failedLogs);
+
+                notifier.notify('zap', 'CSS classes cached (click to cache again)');
+
+                return resolve();
+            });
+        } catch (error) {
+            console.error('Failed while looping through the documents to cache the classes definitions:', error);
+            notifier.notify('alert', 'Failed to cache the CSS classes on the workspace (click for another attempt)');
+            return reject(error);
         }
-
-        uniqueDefinitions = _.uniqBy(definitions, (x) => x.className);
-        notifier.notify('zap', 'CSS classes cached (click to cache again)');
-    } catch (error) {
-        console.error('HTML CSS Class Completion: Failed while looping through the documents to cache the classes definitions:', error);
-        notifier.notify('alert', 'Failed to cache the CSS classes on the workspace (click for another attempt)');
-    }
-}
-
-function registerCompletionItemProvider(): void {
-    
+    });
 }
 
 export async function activate(context: vscode.ExtensionContext): Promise<void> {
     context.subscriptions.push(vscode.commands.registerCommand('html-css-class-completion.cache', async () => {
         await cache();
     }));
-    
+
     await cache();
 
     context.subscriptions.push(vscode.languages.registerCompletionItemProvider('html', {
@@ -98,4 +121,4 @@ export async function activate(context: vscode.ExtensionContext): Promise<void> 
 export function deactivate(): void {
 }
 
-// TODO: Look for the CSS classes in case a new file is added to the workspace. I think the API does not provide and event for that. Maybe I should consider opening a PR.
+// TODO: Look for CSS class definitions automatically in case a new file is added to the workspace. I think the API does not provide and event for that. Maybe I should consider opening a PR.
