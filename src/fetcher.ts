cff67ef582e14227eb78c255c0ca99e1eae395ce
pungggi <alessandro.pungitore@gmail.com>
2017-11-18 20:28:16 +0100

    change configuration defaullts

--- src/fetcher.ts
+++ src/fetcher.ts
@@ -3,12 +3,10 @@ import ParseEngineRegistry from './parse-engines/parse-engine-registry';
 
 class Fetcher {
     static async findAllParseableDocuments(): Promise<vscode.Uri[]> {
-        const languages = ParseEngineRegistry.supportedLanguagesIds.join(',');
-
         const includeGlobPattern = vscode.workspace.getConfiguration().get('html-css-class-completion.includeGlobPattern');
         const excludeGlobPattern = vscode.workspace.getConfiguration().get('html-css-class-completion.excludeGlobPattern');
 
-        return await vscode.workspace.findFiles(`${includeGlobPattern}.{${languages}}`, `${excludeGlobPattern}`);
+        return await vscode.workspace.findFiles(`${includeGlobPattern}`, `${excludeGlobPattern}`);
     }
 }
 
